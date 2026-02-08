b064074c9022c3aaabeba1f05955d7837b7dbc34
Nik <nikita.dovda.lg@gmail.com>
2026-02-08 13:47:23 +0200

    [*] fix collider bug

--- Assets/_Game/scripts/Car/Core/CarInstaller.cs
+++ Assets/_Game/scripts/Car/Core/CarInstaller.cs
@@ -24,6 +24,7 @@ public class CarInstaller : MonoInstaller
     {
         Container.BindInterfacesAndSelfTo<CarStateMachine>().AsSingle();
         Container.Bind<IState>().To<EmptyCarState>().AsSingle();
+        Container.Bind<IState>().To<EnterCarState>().AsSingle();
         Container.Bind<IState>().To<DrivingCarState>().AsSingle();
         Container.Bind<IState>().To<ExitCarState>().AsSingle();
     }
